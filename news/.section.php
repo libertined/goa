efe36b42676371d032030eb67236f642043c67c6
jupoo <polintseva.y@party-masters.ru>
2016-08-24 22:32:40 +0300

    Add to Jupoo

---
+++ news/.section.php
@@ -0,0 +1,8 @@
+<?
+$sSectionName = "Новости";
+$arDirProperties = Array(
+   "robots" => "index, follow",
+   "MAIN_TITLE" => "Новости",
+   "PAGE_IMAGE" => "/upload/images/bg_news.jpg"
+);
+?>
---
+++ news/index.php
@@ -0,0 +1,6 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Новости");
+$APPLICATION->SetPageProperty("MAIN_TITLE", "Новости");
+$APPLICATION->SetPageProperty("PAGE_IMAGE", "/upload/images/bg_news.jpg");?>
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
