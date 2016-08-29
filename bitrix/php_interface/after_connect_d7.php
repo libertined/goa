f20d11202782de6157372c8c680bf599665257a7
Mitya <mail@ae-studio.ru>
2016-08-29 13:30:08 +0300

    Change MySQL settings as recommended

--- bitrix/php_interface/after_connect_d7.php
+++ bitrix/php_interface/after_connect_d7.php
@@ -2,4 +2,6 @@
 $connection = \Bitrix\Main\Application::getConnection();
 $connection->queryExecute("SET NAMES 'utf8'");
 $connection->queryExecute('SET collation_connection = "utf8_unicode_ci"');
+$connection = Bitrix\Main\Application::getConnection();
+$connection->queryExecute("SET sql_mode=''");
 ?>
