08b2bb73c35be93135586dd3b9f0e53f40a66988
jupoo <polintseva.y@party-masters.ru>
2016-09-01 14:28:29 +0300

    commit

--- local/templates/.default/components/bitrix/news.list/top_main/template.php
+++ local/templates/.default/components/bitrix/news.list/top_main/template.php
@@ -12,13 +12,32 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
+
 <div class="right-side__top5-wrap">
   <div class="row row--padd30">
     <div class="row__inner">
-      <div class="col-12-5"><a href="<?=$arResult["ITEMS"][0]["DETAIL_PAGE_URL"]?>" class="right-side__top5"><img src="<?=$arResult["ITEMS"][0]["PREVIEW_PICTURE"]["SRC"]?>" alt="" /></a></div>
+      <div class="col-12-5">
+        <a href="<?=$arResult["ITEMS"][0]["DETAIL_PAGE_URL"]?>" class="right-side__top5">
+          <img src="<?=$arResult["ITEMS"][0]["PREVIEW_PICTURE"]["SRC"]?>" alt="" />
+          <div class="right-side__top5-hover-wrap">
+            <div class="vert-middle">
+              <div class="vert-middle__inner">
+                <div class="right-side__top5-hover">Весь гоа за один день</div>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
       <div class="col-12-7">
         <a href="<?=$arResult["ITEMS"][1]["DETAIL_PAGE_URL"]?>" class="right-side__top5">
           <img src="<?=$arResult["ITEMS"][1]["PREVIEW_PICTURE"]["SRC"]?>" alt="" />
+          <div class="right-side__top5-hover-wrap">
+            <div class="vert-middle">
+              <div class="vert-middle__inner">
+                <div class="right-side__top5-hover">Биджапур</div>
+              </div>
+            </div>
+          </div>
         </a>
         <div class="right-side__top5-pos">
           <div class="row row--padd30">
@@ -30,16 +49,37 @@ $this->setFrameMode(true);
                     <div class="col-12-4">
                       <a href="<?=$arResult["ITEMS"][2]["DETAIL_PAGE_URL"]?>" class="right-side__top5">
                         <img src="<?=$arResult["ITEMS"][2]["PREVIEW_PICTURE"]["SRC"]?>" alt="" />
+                        <div class="right-side__top5-hover-wrap">
+                          <div class="vert-middle">
+                            <div class="vert-middle__inner">
+                              <div class="right-side__top5-hover">Биджапур</div>
+                            </div>
+                          </div>
+                        </div>
                       </a>
                     </div>
                     <div class="col-12-4">
                       <a href="<?=$arResult["ITEMS"][3]["DETAIL_PAGE_URL"]?>" class="right-side__top5">
                         <img src="<?=$arResult["ITEMS"][3]["PREVIEW_PICTURE"]["SRC"]?>" alt="" />
+                        <div class="right-side__top5-hover-wrap">
+                          <div class="vert-middle">
+                            <div class="vert-middle__inner">
+                              <div class="right-side__top5-hover">Биджапур</div>
+                            </div>
+                          </div>
+                        </div>
                       </a>
                     </div>
                     <div class="col-12-4">
                       <a href="<?=$arResult["ITEMS"][4]["DETAIL_PAGE_URL"]?>" class="right-side__top5">
                         <img src="<?=$arResult["ITEMS"][4]["PREVIEW_PICTURE"]["SRC"]?>" alt="" />
+                        <div class="right-side__top5-hover-wrap">
+                          <div class="vert-middle">
+                            <div class="vert-middle__inner">
+                              <div class="right-side__top5-hover">Биджапур</div>
+                            </div>
+                          </div>
+                        </div>
                       </a>
                     </div>
                   </div>
