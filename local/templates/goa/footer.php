b9f790526ea8cb4325e3099eb4faaa5cf371dc05
jupoo <polintseva.y@party-masters.ru>
2016-09-05 16:14:07 +0300

    commit

--- local/templates/goa/footer.php
+++ local/templates/goa/footer.php
@@ -19,58 +19,109 @@
 </div>
 
 <footer class="page-footer">
-  <div class="page-footer__top">
-    <div class="page-content-inner page-content-inner--w1300">
-      <div class="row">
-        <div class="row__inner">
-          <div class="col-12-4">
-            <div class="page-footer__title">Контакты</div>
-            <div class="page-footer__contact">
-              <div class="page-footer__contact-title">Индия</div>
-              <div class="page-footer__contact-text">
-                <p onclick="location.href='tel:+918888717922'">+91 8888 71 79 22</p>
-                <p onclick="location.href='tel:+918888704005'">+91 8888 70 40 05</p>
-                <p onclick="location.href='tel:+917875009944'">+91 7875 00 99 44</p>
+  <div class="visible-adaptive-768">
+    <div class="page-footer__top">
+      <div class="page-content-inner page-content-inner--w1300">
+        <div class="row">
+          <div class="row__inner">
+            <div class="col-12-4">
+              <div class="page-footer__title">Контакты</div>
+              <div class="page-footer__contact">
+                <div class="page-footer__contact-title">Индия</div>
+                <div class="page-footer__contact-text">
+                  <p onclick="location.href='tel:+918888717922'">+91 8888 71 79 22</p>
+                  <p onclick="location.href='tel:+918888704005'">+91 8888 70 40 05</p>
+                  <p onclick="location.href='tel:+917875009944'">+91 7875 00 99 44</p>
+                </div>
+              </div>
+
+              <div class="page-footer__contact">
+                <div class="page-footer__contact-title">Россия</div>
+                <div class="page-footer__contact-text">
+                  <p onclick="location.href='tel:+79817717922'">+7 981 771 79 22</p>
+                </div>
+              </div>
+
+              <div class="page-footer__contact">
+                <div class="page-footer__contact-title">E-mail</div>
+                <div class="page-footer__contact-text">
+                  <p><a href="mailto:info@tvoygoa.ru" class="page-footer--contact-link">info@tvoygoa.ru</a></p>
+                </div>
               </div>
             </div>
 
-            <div class="page-footer__contact">
-              <div class="page-footer__contact-title">Россия</div>
-              <div class="page-footer__contact-text">
-                <p onclick="location.href='tel:+79817717922'">+7 981 771 79 22</p>
+            <div class="col-12-5">
+              <div class="page-footer__title">Присоединяйтесь к нам</div>
+              <div class="page-footer__socials">
+                <a href="" class="page-footer__social-item page-footer--social-ig"><object type="image/svg+xml" data="<?=SITE_TEMPLATE_PATH?>/svg/instagram.svg">Your browser does not support SVGs</object></a>
+                <a href="" class="page-footer__social-item page-footer--social-fb"><object type="image/svg+xml" data="<?=SITE_TEMPLATE_PATH?>/svg/instagram.svg">Your browser does not support SVGs</object></a>
+                <a href="" class="page-footer__social-item page-footer--social-vk"><object type="image/svg+xml" data="<?=SITE_TEMPLATE_PATH?>/svg/instagram.svg">Your browser does not support SVGs</object></a>
               </div>
             </div>
 
-            <div class="page-footer__contact">
-              <div class="page-footer__contact-title">E-mail</div>
-              <div class="page-footer__contact-text">
-                <p><a href="mailto:info@tvoygoa.ru" class="page-footer--contact-link">info@tvoygoa.ru</a></p>
+            <div class="col-12-3">
+              <div class="page-footer__dev-wrap">
+                <div class="page-footer__dev"><span>Разработка -</span> <a href="">AE! studio</a></div>
+                <div class="page-footer__dev"><span>Дизайн от</span> <a href="">Pixeljam</a></div>
               </div>
-            </div>
-          </div>
-
-          <div class="col-12-4">
-            <div class="page-footer__title">Присоединяйтесь к нам</div>
-            <div class="page-footer__socials">
-              <a href="" class="page-footer__social-item page-footer--social-ig"><object type="image/svg+xml" data="<?=SITE_TEMPLATE_PATH?>/svg/instagram.svg">Your browser does not support SVGs</object></a>
-              <a href="" class="page-footer__social-item page-footer--social-fb"><object type="image/svg+xml" data="<?=SITE_TEMPLATE_PATH?>/svg/instagram.svg">Your browser does not support SVGs</object></a>
-              <a href="" class="page-footer__social-item page-footer--social-vk"><object type="image/svg+xml" data="<?=SITE_TEMPLATE_PATH?>/svg/instagram.svg">Your browser does not support SVGs</object></a>
-            </div>
-          </div>
-
-          <div class="col-12-4">
-            <div class="page-footer__dev-wrap">
-              <div class="page-footer__dev"><span>Разработка -</span> <a href="">AE! studio</a></div>
-              <div class="page-footer__dev"><span>Дизайн от</span> <a href="">Pixeljam</a></div>
             </div>
           </div>
         </div>
       </div>
     </div>
+
+    <div class="page-footer__copyright">
+      <div class="page-content-inner page-content-inner--w1300">
+        <div class="page-footer__copyright-inner">© 2011-2016 «Твой Гоа»</div>
+      </div>
+    </div>
   </div>
-  <div class="page-footer__copyright">
-    <div class="page-content-inner page-content-inner--w1300">
-      <div class="page-footer__copyright-inner">© 2011-2016 «Твой Гоа»</div>
+
+  <div class="visible-adaptive-show-768">
+    <div class="page-footer__top">
+      <div class="page-content-inner page-content-inner--w1300">
+        <div class="row">
+          <div class="row__inner">
+            <div class="col-12-9">
+              <nav class="page-footer__nav">
+                <ul class="page-footer__nav-list">
+                  <li class="page-footer__nav-item page-footer--nav-current"><a href="/rent/">Аренда жилья</a></li>
+                  <li class="page-footer__nav-item"><a href="/advantage/">Наши преимущества</a></li>
+                  <li class="page-footer__nav-item"><a href="/specials/">Специальные предложения</a></li>
+                  <li class="page-footer__nav-item"><a href="/excursion/132/">Сводная таблица цен</a></li>
+                  <li class="page-footer__nav-item"><a href="/interesting/">Интересно об Индии</a></li>
+                  <li class="page-footer__nav-item"><a href="/advances/">Советы туристам в Гоа</a></li>
+                  <li class="page-footer__nav-item"><a href="/news/">Новости</a></li>
+                  <li class="page-footer__nav-item"><a href="/beach/">Пляжи Гоа</a></li>
+                  <li class="page-footer__nav-item"><a href="/ind_info/">Информация об Индии</a></li>
+                  <li class="page-footer__nav-item"><a href="/goa_info/">Информация о Гоа</a></li>
+                </ul>
+              </nav>
+            </div>
+            <div class="col-12-3 pos-right">
+              <div class="page-footer__socials">
+                <a href="" class="page-footer__social-item page-footer--social-ig"><object type="image/svg+xml" data="<?=SITE_TEMPLATE_PATH?>/svg/instagram.svg">Your browser does not support SVGs</object></a>
+                <a href="" class="page-footer__social-item page-footer--social-fb"><object type="image/svg+xml" data="<?=SITE_TEMPLATE_PATH?>/svg/instagram.svg">Your browser does not support SVGs</object></a>
+                <a href="" class="page-footer__social-item page-footer--social-vk"><object type="image/svg+xml" data="<?=SITE_TEMPLATE_PATH?>/svg/instagram.svg">Your browser does not support SVGs</object></a>
+                <a href="" class="page-footer__social-item page-footer--social-mail"><object type="image/svg+xml" data="<?=SITE_TEMPLATE_PATH?>/svg/instagram.svg">Your browser does not support SVGs</object></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="page-footer__copyright">
+      <div class="page-content-inner page-content-inner--w1300">
+        <div class="page-footer__copyright-inner">
+          <div class="row">
+            <div class="row__inner">
+              <div class="col-12-6">© 2016 Твой Гоа</div>
+              <div class="col-12-6 pos-right">Разработка <a href="">AE! studio</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </footer>
