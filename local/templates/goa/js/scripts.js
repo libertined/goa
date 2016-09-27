3438e71520349793f89c7ef4a802f44403b1358e
jupoo <polintseva.y@party-masters.ru>
2016-09-28 00:09:49 +0300

    commit

--- local/templates/goa/js/scripts.js
+++ local/templates/goa/js/scripts.js
@@ -168,4 +168,5 @@ $(document).ready( function() {
 		select_wrap.find('.pseudo-select__real option[value="'+data_value+'"]').attr("selected","selected");
 		select_wrap.removeClass("pseudo-select--show");
 	});
+
 });
