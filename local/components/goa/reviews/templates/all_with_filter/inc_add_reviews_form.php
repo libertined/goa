9f220ebacab8e810778c1c039907604ed77ef8a7
Mark <dev@dermanov.ru>
2016-10-15 00:25:15 +0300

    reviews list template, step 1

---
+++ local/components/goa/reviews/templates/all_with_filter/inc_add_reviews_form.php
@@ -0,0 +1,74 @@
+
+<div class="modal-window" id="modal-review">
+    <div class="modal-window__body">
+        <form action="" method="post" name="">
+            <div class="form form--grey form--w670 form--center">
+                <div class="form__header">Добавление отзыва</div>
+                <div class="marg-b-15">
+                    <div class="form__wrap">
+                        <label for="label-review-ex" class="form__title">Экскурсия</label>
+                        <div class="form__field">
+                            <div class="pseudo-select">
+                                <div class="pseudo-select__text">Выберите экскурсию</div>
+                                <div class="pseudo-select__wrap scroll-pane">
+                                    <ul class="pseudo-select__list">
+                                        <li class="pseudo-select__option" data-value="1">Отдых на пляжах Палолем, Кола и старинный форт Кабо де Рама</li>
+                                        <li class="pseudo-select__option" data-value="2">Отдых на пляжах Палолем, Кола и старинный форт Кабо де Рама</li>
+                                        <li class="pseudo-select__option" data-value="3">Весь ГОА за 1 день</li>
+                                        <li class="pseudo-select__option" data-value="4">Отдых на пляжах Палолем, Кола и старинный форт Кабо де Рама</li>
+                                        <li class="pseudo-select__option" data-value="5">Отдых на пляжах Палолем, Кола и старинный форт Кабо де Рама</li>
+                                    </ul>
+                                </div>
+                                <select name="" class="pseudo-select__real" id="label-review-ex">
+                                    <option value="1">Отдых на пляжах Палолем, Кола и старинный форт Кабо де Рама</option>
+                                    <option value="2">Отдых на пляжах Палолем, Кола и старинный форт Кабо де Рама</option>
+                                    <option value="3">Весь ГОА за 1 день</option>
+                                    <option value="4">Отдых на пляжах Палолем, Кола и старинный форт Кабо де Рама</option>
+                                    <option value="5">Отдых на пляжах Палолем, Кола и старинный форт Кабо де Рама</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="marg-b-15">
+                    <div class="form__wrap">
+                        <label for="label-review-name" class="form__title">Представьтесь, пожалуйста</label>
+                        <div class="form__field"><input id="label-review-name" placeholder="" name="" value="" type="text" class="form__input" /></div>
+                    </div>
+                </div>
+
+                <div class="marg-b-15">
+                    <div class="form__wrap">
+                        <label for="label-review-soc" class="form__title">Профиль Вконтакте или Facebook</label>
+                        <div class="form__field"><input id="label-review-soc" placeholder="" name="" value="" type="text" class="form__input" /></div>
+                    </div>
+                </div>
+
+                <div class="marg-b-15">
+                    <div class="form__wrap">
+                        <label for="label-review-text" class="form__title">Текст отзыва</label>
+                        <div class="form__field"><textarea id="label-review-text" class="form__textarea"></textarea></div>
+                    </div>
+                </div>
+
+                <div class="marg-b-15">
+                    <div class="form__wrap">
+                        <div class="form__title">При желании вы можете прикрепить к отзыву несколько фото</div>
+                        <div class="form__field">
+                            <div class="form__file">
+                                <div class="form__file-icon"><object type="image/svg+xml" data="<?=SITE_TEMPLATE_PATH?>/svg/faceless.svg">Your browser does not support SVGs</object></div>
+                                <div class="form__file-text">Перетащите фото сюда или нажмите для выбора на компьютере</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form__wrap">
+                    <div class="form__title visible-adaptive-768">&nbsp;</div>
+                    <div class="form__field pos-right"><button type="submit" name="" class="btn col-adaptive-768">Отправить</button></div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
