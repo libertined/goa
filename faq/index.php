0659d8d697a194729d6039850455ec3d30914bc8
Dmitrii Kaplunov <mail@ae-studio.ru>
2016-11-17 21:56:41 +0300

    start faq section

--- faq/index.php
+++ faq/index.php
@@ -1,7 +1,52 @@
 <?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetPageProperty("MAIN_TITLE", "Отдых на пляжах Палолем, Кола и старинный форт Кабо де Рама");
+$APPLICATION->SetPageProperty("MAIN_TITLE", "Часто задаваемые вопросы");
 $APPLICATION->SetPageProperty("PAGE_IMAGE", "/upload/images/bg_exs_1.jpg");
 $APPLICATION->SetTitle("FAQ");?>
-
+<h2>Вопросы</h2>
+<a href="#" class="btn">Задать вопрос</a>
+<ul>
+    <li><a href="#1">Когда лучше всего ехать в Гоа?</a></li>
+    <li><a href="#2">Как купить местную сим-карту?</a></li>
+    <li><a href="#3">Южный Гоа или Северный Гоа?</a></li>
+    <li><a href="#4">Что с собой брать?</a></li>
+    <li><a href="#5">Банковские карточки и обмен валюты?</a></li>
+</ul>
+<h2>Ответы</h2>
+<ul>
+    <li>
+        <a href="" id="1"></a>
+        <strong>Когда лучше всего ехать в Гоа?</strong>
+        <div class="text-answ">
+            <p>В Гоа лучше ехать с середины октября и до конца апреля.
+В начале сезона немного влажно, зато природа потрясающе насыщена красками. Средняя температура воздуха 25-35С и очень комфортная температура морской воды. Самое лучшее время для поездки в Гоа декабрь-февраль.</p>
+            <p>Гоа - уникальное место. Весь сезон вы можете наслаждаться солнцем, безоблачным небом и «постоянством» рассветов и закатов -  всегда в одно и то же время.</p>
+        </div>
+        <p class="to-content">к содержанию</p>
+    </li>
+    <li>
+        <a href=""></a>
+        <strong></strong>
+        <div class="text-answ"></div>
+        <p class="to-content"></p>
+    </li>
+    <li>
+        <a href=""></a>
+        <strong></strong>
+        <div class="text-answ"></div>
+        <p class="to-content"></p>
+    </li>
+    <li>
+        <a href=""></a>
+        <strong></strong>
+        <div class="text-answ"></div>
+        <p class="to-content"></p>
+    </li>
+    <li>
+        <a href=""></a>
+        <strong></strong>
+        <div class="text-answ"></div>
+        <p class="to-content"></p>
+    </li>
+</ul>
 
 <?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
