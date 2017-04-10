cab4f044f98fc3bc0522e8ab2325a43317d67f2c
Dmitrii Kaplunov <mail@ae-studio.ru>
2017-04-10 15:29:05 +0400

    Added content for summary page

--- excursion/summary/index.php
+++ excursion/summary/index.php
@@ -2,4 +2,177 @@
 $APPLICATION->SetPageProperty("MAIN_TITLE", "Сводная таблица цен");
 $APPLICATION->SetPageProperty("PAGE_IMAGE", "/upload/images/bg_exs.jpg");
 $APPLICATION->SetTitle("Сводная таблица цен");?>
+<h2>Сводная таблица цен на экскурсии по Гоа и в Индии - незабываемые впечатления по привлекательным ценам</h2>
+<p>* Заказывая экскурсии на сайте вы получаете скидку</p>
+<p>* Дети до 5 лет путешествуют с нами бесплатно (без места)</p>
+<p>* Скидки и акции на экскурсии без русского гида не распространяются.</p>
+<p>* Скидки по акциям не суммируются.</p>
+<p>Цены указаны для экскурсий при отправке из Северного Гоа. Доплата за трансфер для отелей Южного Гоа, для пляжей Мандрем, Морджим, Ашвем, Арамболь 5-10$ за 1 человека (в зависимости от количества человек).</p>
+<p>Доплата за трансфер для пляжей Палолем, Агонда 80$ (трансфер в оба конца) за машину.</p>
+<p>Мы принимаем оплату в любой валюте: доллары, евро, рупии, рубли.</p>
+<p class="summary__warn">Для групп от 6 человек цены устанавливаются индивидуально на все экскурсии!</p>
+<h3 class="right-side__red">Сводная таблица цен на однодневные экскурсии из Гоа</h3>
+<table class="summary">
+    <col class="summary__col summary__col--1">
+    <col class="summary__col summary__col--2">
+    <thead class="summary__header">
+        <tr class="summary__row summary__row--header">
+            <td class="summary__header-text">Название экскурсии</td>
+            <td class="summary__header-text">Продолжительность</td>
+            <td class="summary__header-text">Стоимость<br>на 1 человека</td>
+        </tr>
+    </td>
+    <tbody class="summary__body">
+        <tr class="summary__row">
+            <td class="summary__text" rowspan="3">Животный и растительный мир Гоа. Самый древний храм Гоа</td>
+            <td class="summary__text" rowspan="3">с 7.00 до 17.30</td>
+            <td class="summary__text">50$<br><span class="summary__text--smaller">при заказе через сайт</span></td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text summary__text--col">59$<br><span class="summary__text--smaller">при заказе в Гоа</span></td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text summary__text--col">25$<br><span class="summary__text--smaller">дети до 12 лет</span></td>
+        </tr>
+
+        <tr class="summary__row">
+            <td class="summary__text" rowspan="3">Весь Гоа за один день</td>
+            <td class="summary__text" rowspan="3">c 06.00 до 16.30<br >
+            1 день</td>
+            <td class="summary__text">30$<br><span class="summary__text--smaller">при заказе через сайт</span></td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text summary__text--col">35$<br><span class="summary__text--smaller">при заказе в Гоа</span></td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text summary__text--col">20$<br><span class="summary__text--smaller">дети до 12 лет</span></td>
+        </tr>		
+
+        <tr class="summary__row">
+            <td class="summary__text">Путешествие к тиграм. Водопад Джог Фолс и храм Агорешвара</td>
+            <td class="summary__text">с 4.00 до 22.00<br/>
+            1 день</td>
+            <td class="summary__text">100$</td>
+        </tr>
+
+        <tr class="summary__row">
+            <td class="summary__text" rowspan="3">Панаджи, Старый Гоа и круиз по реке</td>
+            <td class="summary__text" rowspan="3">c 10.00 до 19.30<br >
+            1 день</td>
+            <td class="summary__text">45$<br><span class="summary__text--smaller">при заказе через сайт</span></td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text summary__text--col">50$<br><span class="summary__text--smaller">при заказе в Гоа</span></td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text summary__text--col">25$<br><span class="summary__text--smaller">дети до 12 лет</span></td>
+        </tr>	
+
+        <tr class="summary__row">
+            <td class="summary__text" rowspan="3">Отдых на пляжах Палолем, Кола и старинный форт Кабо де Рама</td>
+            <td class="summary__text" rowspan="3">с 08.00 до 20.30<br >
+            1 день</td>
+            <td class="summary__text">40$<br><span class="summary__text--smaller">при заказе через сайт</span></td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text summary__text--col">45$<br><span class="summary__text--smaller">при заказе в Гоа</span></td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text summary__text--col">20$<br><span class="summary__text--smaller">дети до 12 лет</span></td>
+        </tr>	
+
+        <tr class="summary__row">
+            <td class="summary__text" rowspan="3">Рыбалка, дельфины, барбекю на острове</td>
+            <td class="summary__text" rowspan="3">с 08.00 до 15.30<br >
+            1 день</td>
+            <td class="summary__text">35$<br><span class="summary__text--smaller">при заказе через сайт</span></td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text summary__text--col">35$<br><span class="summary__text--smaller">при заказе в Гоа</span></td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text summary__text--col">35$<br><span class="summary__text--smaller">дети до 12 лет</span></td>
+        </tr>	
+
+        <tr class="summary__row">
+            <td class="summary__text" rowspan="3">Крокодилы</td>
+            <td class="summary__text" rowspan="3">c 08.30 до 13.30<br >
+            1 день</td>
+            <td class="summary__text">30$<br><span class="summary__text--smaller">при заказе через сайт</span></td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text summary__text--col">30$<br><span class="summary__text--smaller">при заказе в Гоа</span></td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text summary__text--col">30$<br><span class="summary__text--smaller">дети до 12 лет</span></td>
+        </tr>	
+
+        <tr class="summary__row">
+            <td class="summary__text">Отдых на пляже Палолем</td>
+            <td class="summary__text">1 день (12 часов)</td>
+            <td class="summary__text summary__text--col">от 65$<br><span class="summary__text--smaller">за машину</span></td>
+        </tr>
+
+        <tr class="summary__row">
+            <td class="summary__text">Панаджи - столица Гоа (шоппинг)</td>
+            <td class="summary__text">Любое время<br>(5 часов)</td>
+            <td class="summary__text summary__text--col">35$<br><span class="summary__text--smaller">за машину до 7 человек</span></td>
+        </tr>
+
+        <tr class="summary__row">
+            <td class="summary__text">Дневной / ночной рынок</td>
+            <td class="summary__text">5 часов среда / суббота</td>
+            <td class="summary__text summary__text--col">10$</td>
+        </tr>
+
+        <tr class="summary__row">
+            <td class="summary__text">Древние индуистские храмы Гоа</td>
+            <td class="summary__text">c 08.00 до 18.00<br>
+            1 день</td>
+            <td class="summary__text summary__text--col">50$</td>
+        </tr>
+
+        <tr class="summary__row">
+            <td class="summary__text" rowspan="3">Гокарна, Мурудешвар, пляж Ом</td>
+            <td class="summary__text" rowspan="3">с 04.45 до 20.00<br >
+            1 день</td>
+            <td class="summary__text">45$<br><span class="summary__text--smaller">при заказе через сайт</span></td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text summary__text--col">50$<br><span class="summary__text--smaller">при заказе в Гоа</span></td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text summary__text--col">30$<br><span class="summary__text--smaller">дети до 12 лет</span></td>
+        </tr>	
+
+        <tr class="summary__row">
+            <td class="summary__text" rowspan="3">Houseboat - дом на воде</td>
+            <td class="summary__text">дневная прогулка с 10.00 до 15.00</td>
+            <td class="summary__text">75$</td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text">вечерняя прогулка с 16.00 до 21.00</td>
+            <td class="summary__text summary__text--col">75$</td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text">поездка на сутки с 17.00 до 13.00 второго дня</td>
+            <td class="summary__text summary__text--col">130$<br><span class="summary__text--smaller">номер на двоих</span></td>
+        </tr>	
+
+        <tr class="summary__row">
+            <td class="summary__text" rowspan="3">Бадами, Паттадакал, Айхоле</td>
+            <td class="summary__text" rowspan="3">с 04.00 до 22.00<br >
+            1 день</td>
+            <td class="summary__text">90$<br><span class="summary__text--smaller">при заказе через сайт</span></td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text summary__text--col">100$<br><span class="summary__text--smaller">при заказе в Гоа</span></td>
+        </tr>
+        <tr class="summary__row">
+            <td class="summary__text summary__text--col">50$<br><span class="summary__text--smaller">дети до 12 лет</span></td>
+        </tr>	
+
+    </tbody>
+</table>
+
 <?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
