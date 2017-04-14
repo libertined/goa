f0b6c122442235d4c245a66024b6684e935c18a4
Dmitrii Kaplunov <mail@ae-studio.ru>
2017-04-14 20:20:39 +0400

    try new section

---
+++ promo/index.php
@@ -0,0 +1,20 @@
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("MAIN_TITLE", "Экскурсии");
+$APPLICATION->SetPageProperty("PAGE_IMAGE", "/upload/images/bg_exs.jpg");
+$APPLICATION->SetTitle("Экскурсии");
+$APPLICATION->SetAdditionalCSS("/promo/style.css");?>
+
+	<section class="promo">
+		<article class="promo__item clearfix">
+			<a class="promo__link-img" href="#"><img class="promo__photo" src="promo-1.jpg" alt=""></a>
+			<a class="promo__title" href="#">Заголовок спецпредложения</a>
+			<p class="promo__description">Текст спецпредложения здесь Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere dolorum aut amet quos inventore, quas ex, quae fuga et reprehenderit quia iure eum tempore eius perferendis deserunt maxime quo. Dolore? Текст спецпредложения здесь Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere dolorum aut amet quos inventore, quas ex, quae fuga et reprehenderit quia iure eum tempore eius perferendis deserunt maxime quo. Dolore? Текст спецпредложения здесь Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere dolorum aut amet quos inventore, quas ex, quae fuga et reprehenderit quia iure eum tempore eius perferendis deserunt maxime quo. Dolore? Текст спецпредложения здесь Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere dolorum aut amet quos inventore, quas ex, quae fuga et reprehenderit quia iure eum tempore eius perferendis deserunt maxime quo. Dolore? Текст спецпредложения здесь Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere dolorum aut amet quos inventore, quas ex, quae fuga et reprehenderit quia iure eum tempore eius perferendis deserunt maxime quo. Dolore? Текст спецпредложения здесь Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere dolorum aut amet quos inventore, quas ex, quae fuga et reprehenderit quia iure eum tempore eius perferendis deserunt maxime quo. Dolore? Завершающий текст здесь</p>
+		</article>
+
+		<article class="promo__item clearfix">
+			<a class="promo__link-img" href="#"><img class="promo__photo" src="promo-1.jpg" alt=""></a>
+			<a class="promo__title" href="#">Заголовок спецпредложения</a>
+			<p class="promo__description">Текст спецпредложения здесь Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere dolorum aut amet quos inventore, quas ex, quae fuga et reprehenderit quia iure eum tempore eius perferendis deserunt maxime quo. Dolore?</p>
+		</article>
+	</section>
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
