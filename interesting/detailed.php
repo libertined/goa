24b2cdeca53f440354c71d760b16f2b301c0dd8e
Dmitrii Kaplunov <mail@ae-studio.ru>
2017-07-03 00:21:40 +0400

    Added styles to detailed page

--- interesting/detailed.php
+++ interesting/detailed.php
@@ -6,13 +6,13 @@ $APPLICATION->SetPageProperty("PAGE_IMAGE", "/upload/images/bg_exs.jpg");?>
     <article class="interesting-detailed__content">
         <div class="interesting-detailed__header">Заголовок статьи</div>
         <p class="interesting-detailed__text">Детальный текст здесь Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, provident facilis tempora ut doloribus adipisci molestias et commodi veritatis cupiditate reprehenderit quibusdam dolorum esse saepe illum? Molestiae, natus necessitatibus aliquid.</p>
+        <a href="/interesting/" class="interesting-detailed__back">Назад к списку</a>
     </article>
     <aside class="interesting-detailed__links">
         <p class="interesting-detailed__links-header">Разделы</p>
         <p class="interesting-detailed__labels">
             <a href="" class="interesting-detailed__label">метка 1</a>, <a href="" class="interesting-detailed__label">метка 2</a>, <a href="" class="interesting-detailed__label">метка 3</a>
         </p>
-        <a href="/interesting/" class="interesting-detailed__back">Назад к списку</a>
         <div class="right-side__share">
             <div class="right-side__share-title">Поделиться:</div>
             <div class="right-side__share-body">
