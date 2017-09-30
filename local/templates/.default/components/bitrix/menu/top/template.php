ac6cdd4476723cea247b4fe1e69bab51928caf09
Mitya <mail@ae-studio.ru>
2017-09-30 20:46:59 +0300

    Fix adaptive menu

--- local/templates/.default/components/bitrix/menu/top/template.php
+++ local/templates/.default/components/bitrix/menu/top/template.php
@@ -21,23 +21,23 @@
 	<div class="visible-adaptive-show-768">
 		<div class="page-header__menu-adaptive">
 			<div class="page-header__menu-adaptive-btn js-menu-adaptive-btn"></div>
-			<nav class="main-nav-adaptive js-menu-adaptive-list">
-				<a href="" class="main-nav-adaptive__close js-menu-adaptive-close"></a>
-				<ul class="main-nav-adaptive__list">
-					<?
-					foreach($arResult as $arItem):
-						if($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1)
-							continue;
-						?>
-						<?if($arItem["SELECTED"]):?>
-						<li class="main-nav-adaptive__item main-nav-adaptive--item-current"><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
-					<?else:?>
-						<li class="main-nav-adaptive__item"><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
-					<?endif?>
-
-					<?endforeach?>
-				</ul>
-			</nav>
 		</div>
+		<nav class="main-nav-adaptive js-menu-adaptive-list">
+			<a href="" class="main-nav-adaptive__close js-menu-adaptive-close"></a>
+			<ul class="main-nav-adaptive__list">
+				<?
+				foreach($arResult as $arItem):
+					if($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1)
+						continue;
+					?>
+					<?if($arItem["SELECTED"]):?>
+					<li class="main-nav-adaptive__item main-nav-adaptive--item-current"><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
+				<?else:?>
+					<li class="main-nav-adaptive__item"><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a></li>
+				<?endif?>
+
+				<?endforeach?>
+			</ul>
+		</nav>
 	</div>
 <?endif?>
