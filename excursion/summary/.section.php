23981062df01c5a2fa29a748229923980a7049cc
Dmitrii Kaplunov <mail@ae-studio.ru>
2018-03-07 16:18:00 +0300

    seo

--- excursion/summary/.section.php
+++ excursion/summary/.section.php
@@ -2,7 +2,7 @@
 $sSectionName = "Сводная таблица цен";
 $arDirProperties = array(
    "robots" => "index, follow",
-   "MAIN_TITLE" => "Сводная таблица цен",
+   "MAIN_TITLE" => "Сводная таблица цен на экскурсии Гоа",
    "PAGE_IMAGE" => "/upload/images/bg_exs.jpg"
 );
 ?>
